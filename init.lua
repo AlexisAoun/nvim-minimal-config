8d6fa5bb80d4f5b9ce20a7c337e9efff3acac259
Alexis Aoun <alexis.aoun@hotmail.com>
2023-11-12 23:33:19 +0100

    wip

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- install package manager if not installed
+--install package manager if not installed
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -35,6 +35,69 @@ require('lazy').setup({
 		}
 	        require 'nordic'.load()
 	    end
-	}	
+	},
+  	{
+  	  -- Set lualine as statusline
+  	  'nvim-lualine/lualine.nvim',
+  	  -- See `:help lualine.txt`
+	  commit = "2248ef254d0a1488a72041cfb45ca9caada6d994",
+  	  opts = {
+  	    options = {
+  	      icons_enabled = false,
+  	      theme = 'nordic',
+  	      component_separators = '|',
+  	      section_separators = '',
+  	    },
+  	  },
+  	},
+	{ 'numToStr/Comment.nvim', opts = {} },
+	{ 'folke/which-key.nvim', 
+	  event = "VeryLazy",
+  	  init = function()
+  	  	vim.o.timeout = true
+  	  	vim.o.timeoutlen = 300
+  	  end,
+	  opts = {}
+	},
+	{
+    -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      on_attach = function(bufnr)
+        vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+
+        -- don't override the built-in and fugitive keymaps
+        local gs = package.loaded.gitsigns
+        vim.keymap.set({ 'n', 'v' }, ']c', function()
+          if vim.wo.diff then
+            return ']c'
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return '<Ignore>'
+        end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
+        vim.keymap.set({ 'n', 'v' }, '[c', function()
+          if vim.wo.diff then
+            return '[c'
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return '<Ignore>'
+        end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+      end,
+    },
+  },
+
 })
 
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
