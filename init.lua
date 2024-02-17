1831e44609b152ddd7ffded2b5f2717413167b4c
Alexis Aoun <alexis.aoun@hotmail.com>
2024-02-17 11:24:31 +0000

    wip

--- init.lua
+++ init.lua
@@ -20,11 +20,12 @@ require('lazy').setup({
 
 	-- colorscheme
 	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+
   	{
   	  -- Set lualine as statusline
   	  'nvim-lualine/lualine.nvim',
   	  -- See `:help lualine.txt`
-	  commit = "2248ef254d0a1488a72041cfb45ca9caada6d994",
+	  commit = "7d131a8d3ba5016229e8a1d08bf8782acea98852",
   	  opts = {
   	    options = {
   	      icons_enabled = false,
@@ -34,8 +35,14 @@ require('lazy').setup({
   	    },
   	  },
   	},
-	{ 'numToStr/Comment.nvim', opts = {} },
+
+	{ 'numToStr/Comment.nvim', 
+	  commit = "0236521ea582747b58869cb72f70ccfa967d2e89",
+	  opts = {},
+  	},
+
 	{ 'folke/which-key.nvim', 
+	  commit="4433e5ec9a507e5097571ed55c02ea9658fb268a",
 	  event = "VeryLazy",
   	  init = function()
   	  	vim.o.timeout = true
@@ -43,9 +50,11 @@ require('lazy').setup({
   	  end,
 	  opts = {}
 	},
+
 	{
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
+    commit="2c2463dbd82eddd7dbab881c3a62cfbfbe3c67ae",
     opts = {
       -- See `:help gitsigns.txt`
       signs = {
@@ -82,6 +91,61 @@ require('lazy').setup({
     },
   },
 
+  -- NOTE: This is where your plugins related to LSP can be installed.
+  --  The configuration is done below. Search for lspconfig to find it below.
+  {
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      { 'williamboman/mason.nvim', config = true },
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim', opts = {} },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+    },
+  },
+
+  {
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+          -- Build Step is needed for regex support in snippets
+          -- This step is not supported in many windows environments
+          -- Remove the below condition to re-enable on windows
+          if vim.fn.has 'win32' == 1 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+      },
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
+  },
+
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+  },
 })
 
 vim.o.background = "dark" -- or "light" for light mode
@@ -114,3 +178,8 @@ vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.relativenumber=true
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
