19abdb3e2c505501ed1f4c519c5ded94440e9c51
Alexis Aoun <alexis.aoun@hotmail.com>
2024-03-16 22:46:14 +0000

    fix comment mapping

--- init.lua
+++ init.lua
@@ -519,7 +519,7 @@ cmp.setup {
 }
 
 require('Comment').setup({
-  opleader = { line = 'c' }
+  opleader = { line = 'gc' }
 })
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
